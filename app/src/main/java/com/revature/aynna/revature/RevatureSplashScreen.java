2a4a634ddf23b62045917904fbb89f575d8b1828
jeevavighnesh <jeevavignesh@gmail.com>
2017-02-20 22:26:17 +0530

    Refined the animation to a much better level. And also removed the UgLy GIF

--- app/src/main/java/com/revature/aynna/revature/RevatureSplashScreen.java
+++ app/src/main/java/com/revature/aynna/revature/RevatureSplashScreen.java
@@ -3,28 +3,32 @@ package com.revature.aynna.revature;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
 public class RevatureSplashScreen extends AppCompatActivity {
 
     private static final String TAG = "Splash Screen";
-    private View gifLoading;
+    private View logo;
+    private View logIn;
+    private View register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_revature_splash_screen);
-        gifLoading = findViewById(R.id.revatureLoadingGif);
+        logIn = findViewById(R.id.logIn);
+        register = findViewById(R.id.register);
+        logo = findViewById(R.id.revatureLogo);
         long SPLASH_SCREEN_TIME = 3000L;
-        Log.v(TAG, "Width = " + gifLoading.getWidth() + "\nHeight = " + gifLoading.getHeight());
-        animate();
+
+        logIn.setVisibility(View.INVISIBLE);
+        register.setVisibility(View.INVISIBLE);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                setContentView(R.layout.activity_revature_home);
+                animate();
 
             }
         }, SPLASH_SCREEN_TIME);
@@ -33,9 +37,13 @@ public class RevatureSplashScreen extends AppCompatActivity {
 
     private void animate() {
 
-        gifLoading.setScaleX(1);
-        gifLoading.setScaleX(1);
-        gifLoading.animate().scaleX(0).scaleY(0).setDuration(1000).setStartDelay(1000);
+        logo.animate().translationY(-100).setDuration(500).setStartDelay(600);
+
+        logIn.setVisibility(View.VISIBLE);
+        register.setVisibility(View.VISIBLE);
+
+        register.animate().translationY(-50).setDuration(500).setStartDelay(500);
+        logIn.animate().translationY(-150).setDuration(400).setStartDelay(600);
 
     }
 }
