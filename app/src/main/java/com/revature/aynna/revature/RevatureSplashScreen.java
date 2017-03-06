53305b9899c9ef942659d6d926e8f5854d6af3fe
jeevavighnesh <jeevavignesh@gmail.com>
2017-03-06 15:47:19 +0530

    Added log in and register intents and also added splash screen animation

--- app/src/main/java/com/revature/aynna/revature/RevatureSplashScreen.java
+++ app/src/main/java/com/revature/aynna/revature/RevatureSplashScreen.java
@@ -1,8 +1,11 @@
 package com.revature.aynna.revature;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -23,27 +26,49 @@ public class RevatureSplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_revature_splash_screen);
         logIn = findViewById(R.id.logIn);
         register = findViewById(R.id.register);
+        View star = findViewById(R.id.star);
         long SPLASH_SCREEN_TIME = 600L;
 
         logIn.setVisibility(View.INVISIBLE);
         register.setVisibility(View.INVISIBLE);
+        moveIt(star);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                animateOffscreen(logIn, 2f, 3600);
-                animateOffscreen(register, 1f, 3600);
+                animateOffscreen(logIn, 3f, 1800);
+                animateOffscreen(register, 2f, 1800);
 
             }
         }, SPLASH_SCREEN_TIME);
 
     }
 
+    private void moveIt(final View view) {
+
+        Log.v(TAG, "Animating star");
+        float x = 160;
+        float y = 380;
+        Path path = new Path();
+        Log.v(TAG, "x: " + x + "y: " + y);
+
+        path.moveTo(x, y);
+        path.addArc(100, 200, 500, 400, -180, 120);
+        Log.v(TAG, "Animating star");
+
+        ObjectAnimator objectAnimator =
+                ObjectAnimator.ofFloat(view, View.X,
+                        View.Y, path);
+
+        objectAnimator.setDuration(3000);
+        Log.v(TAG, "Animating star");
+        objectAnimator.start();
+    }
+
     private void animateOffscreen(View view, float relativeDistance, int animationDuration) {
 
-//        logo.animate().translationY(-100).setDuration(500).setStartDelay(600);
-
+        Log.v(TAG, "Animating buttons");
         view.setVisibility(View.VISIBLE);
 
         TranslateAnimation translateAnimation = new TranslateAnimation(
@@ -52,6 +77,7 @@ public class RevatureSplashScreen extends AppCompatActivity {
                 Animation.RELATIVE_TO_PARENT, relativeDistance, // from Y
                 Animation.RELATIVE_TO_PARENT, 0f); // to Y
         translateAnimation.setDuration(animationDuration); // milliseconds
+        translateAnimation.start();
         view.startAnimation(translateAnimation);
 
     }
@@ -61,7 +87,9 @@ public class RevatureSplashScreen extends AppCompatActivity {
         Log.v(TAG, "Clicked Register Button");
         Intent register = new Intent(this, Register.class);
         Log.v(TAG, "Starting registration Intent");
-        startActivity(register);
+        ActivityOptionsCompat options = ActivityOptionsCompat.
+                makeSceneTransitionAnimation(this, view, "registerButton");
+        startActivity(register, options.toBundle());
     }
 
     public void logIn(View view) {
@@ -69,7 +97,9 @@ public class RevatureSplashScreen extends AppCompatActivity {
         Log.v(TAG, "Clicked LogIn Button");
         Intent logIn = new Intent(this, LogIn.class);
         Log.v(TAG, "Starting LogIn Intent");
-        startActivity(logIn);
+        ActivityOptionsCompat options = ActivityOptionsCompat.
+                makeSceneTransitionAnimation(this, view, "logInButton");
+        startActivity(logIn, options.toBundle());
 
     }
 }
